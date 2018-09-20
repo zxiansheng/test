1ebbda8af25fd0192df4e44d55118a00fdd3d79b
zxiansheng <421605777@qq.com>
2018-09-20 19:18:57 +0800

    add time

--- app/middleware/uniteresponse.ts
+++ app/middleware/uniteresponse.ts
@@ -1,15 +1,21 @@
 export default () => {
   return async function uniteresponse(ctx, next) {
+    // 获取请求的url,记录请求和响应时间
+    const startTime = Date.now();
     await next();
     // 只有api请求才封装
     // if (!(ctx.response.header['content-type'] &&
     // ctx.response.header['content-type'].search('application/json') >= 0)) {
     //   return;
     // }
+    // 上报请求时间
+    const reportTime = Date.now() - startTime;
+
     ctx.body = {
       data: ctx.body,
       ret_code: 0,
       ret_msg: '',
+      request_time: `${reportTime}ms`,
     };
   };
 };
--- app/router.ts
+++ app/router.ts
@@ -2,7 +2,6 @@ import { Application } from 'egg';
 
 export default (app: Application) => {
   const { controller, router } = app;
-  // const response = app.middleware.response;
 
   router.get('/', controller.home.index);
   router.get('/demo', controller.home.demo);
