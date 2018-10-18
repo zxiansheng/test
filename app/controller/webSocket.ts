31c119b21cea39e0c2c212844c2d5feaff55df4a
zxiansheng <421605777@qq.com>
2018-10-18 17:40:32 +0800

    add arr

--- app/controller/webSocket.ts
+++ app/controller/webSocket.ts
@@ -10,9 +10,21 @@ const a = [];
 // @todo 增加多人聊天发送消息
 const WSSERVICE = ws.createServer((conn) => {
   console.log('New a connection');
+  // 定义消息数组
+  var msg_arr: any [];
   conn.on('text', (str) => {
-    console.log('Recevied' + str);
-    conn.sendText(str.toUpperCase() + '!!!');
+    // 心跳检测返回心跳检测的信息
+    if (str.indexOf('heartTest') === -1) {
+      str = JSON.parse(str);
+      const msgs = str.msg;
+      const users = '用户' + str.uid + '号说';
+      const dataStr = { user: users, msgs };
+      const repData = JSON.stringify(dataStr);
+      msg_arr.push(repData);
+      conn.sendText(repData);
+    } else {
+      conn.sendText('heartTest');
+    }
   });
 
   conn.on('close', (code, reason) => {
